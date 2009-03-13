202d680181d62cec3ceefe6c5a06e6e0a34ca7d8
Andrew Shafer <andrew@reductivelabs.com>
2009-03-12 21:18:45 -0600

    Manage the service and vhosts sites

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,10 @@
 class apache2 {
   package{ [apache2, apache2-threaded-dev]: ensure => installed }
+
+  service{ apache2: ensure => running, hasrestart => true }
+
+  apache::site(default:
+      ensure => absent,
+      require => Package['apache2']
+  }
 }
---
+++ manifests/site.pp
@@ -0,0 +1,22 @@
+define apache2::site($ensure) {
+
+    case $ensure {
+        present: {
+            exec { "enable $name":
+                    command => "/usr/sbin/a2ensite $name",
+                    unless => "/bin/ls /etc/apache2/sites-available | grep '$name'",
+                    notify => Service['apache2']
+                 }
+        }
+        absent: {
+            exec { "disable $name":
+                    command => "/usr/sbin/a2dissite $name",
+                    onlyif => "/bin/ls /etc/apache2/sites-available | grep '$name'",
+                    notify => Service['apache2']
+            }
+        }
+        default: {
+            fail "Invalid 'ensure' value '$ensure' for apach2::vhost"
+        }
+    }
+}
